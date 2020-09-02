35ec3ee8657f0cd2a7c01f65d363ca04e446becc
Agustin Dominguez <gusdomdominguez@gmail.com>
2020-09-02 18:58:38 -0300

    Terminada S20 Crear ruta para modificar categoria

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,5 +1,5 @@
 const server = require('express').Router();
-const { Product } = require('../db.js');
+const { Product , Categories} = require('../db.js');
 
 server.get('/', (req, res, next) => {
 	Product.findAll()
@@ -7,6 +7,21 @@ server.get('/', (req, res, next) => {
 			res.send(products);
 		})
 		.catch(next);
+});
+
+//No se puede testear ya que no esta creada la ruta para añadir categorias.
+server.delete('/delete-category/:idCategory', (req, res, next) => {
+	Categories.destroy({
+		where: {
+		   id: req.params.idCategory
+		}
+	 }).then(function(rowDeleted){
+	   if(rowDeleted === 1){
+		  console.log('Categoria borrada de manera exitosa');
+		}
+	 }, function(err){
+		 console.log(err); 
+	 });
 });
 
 module.exports = server;
