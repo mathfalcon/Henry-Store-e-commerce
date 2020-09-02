0eaf3fd68aed357aed0be1d595b35da350e8a577
Mathias Falcon <mathfalcondj@gmail.com>
2020-09-02 15:35:37 -0300

    Realizado S18-Crear-ruta-para-crear/agregar-Categoria

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,5 +1,5 @@
 const server = require('express').Router();
-const { Product } = require('../db.js');
+const { Product, Categories } = require('../db.js');
 
 server.get('/', (req, res, next) => {
 	Product.findAll()
@@ -7,6 +7,22 @@ server.get('/', (req, res, next) => {
 			res.send(products);
 		})
 		.catch(next);
+});
+
+server.post('/create-category', (req, res, next) => {
+	//Suponiendo que el nombre de la categoria llega como body "name"
+	// y la descripcion como 'description'
+	Categories.findOrCreate({
+		where: {
+			name: req.body.name,
+			description: req.body.description
+		}
+	}).then((category) => { //Sucess handler
+		res.status(200).send(`La categoría ${category[0].dataValues.name} se creó con exito`);
+	}).catch((err) => { //Error Handler
+		res.status(400).send('No se pudo crear la categoría solicitada')
+	})
+		
 });
 
 module.exports = server;
