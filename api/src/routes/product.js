7ddef73a3daadec70be2ee6f096ec530c46c4591
Agustin Dominguez <gusdomdominguez@gmail.com>
2020-09-02 18:39:01 -0300

    Realizada S17 Crear ruta para agregar o sacar categorias de un producto

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,12 +1,34 @@
-const server = require('express').Router();
-const { Product } = require('../db.js');
+const server = require("express").Router();
+const { Product, Categories } = require("../db.js");
 
-server.get('/', (req, res, next) => {
-	Product.findAll()
-		.then(products => {
-			res.send(products);
-		})
-		.catch(next);
+server.get("/", (req, res, next) => {
+  Product.findAll()
+    .then((products) => {
+      res.send(products);
+    })
+    .catch(next);
 });
+
+//No puedo testear si funciona ya que aun no han creado la asociacion de Producto-Categorias
+//Se debe tener una tabla intermedia product-categories
+server.post("/:idProducto/category/:idCategoria", (req, res, next) => {
+  Product.findbyPk(req.params.idProducto).then((producto) => {
+    producto.addCategories(Categories.findbyPk(req.params.idCategoria));
+    const result = Product.findOne({
+      where: { id: req.params.idProducto },
+      include: Categories,
+    });
+  });
+});
+
+server.delete("/:idProducto/category/:idCategoria", (req, res, next) => {
+	Product.findbyPk(req.params.idProducto).then((producto) => {
+	  producto.removeCategories(Categories.findbyPk(req.params.idCategoria));
+	  const result = Product.findOne({
+		where: { id: req.params.idProducto },
+		include: Categories,
+	  });
+	});
+  });
 
 module.exports = server;
