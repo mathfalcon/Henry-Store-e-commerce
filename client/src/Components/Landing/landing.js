e04ee6c05f719dd81da39542d0eaf28d0f5c35d5
Mathias Falcon <mathfalcondj@gmail.com>
2020-09-06 01:08:20 -0300

    0.2, se añadio el renderizado automatico de catálogo

--- client/src/Components/Landing/landing.js
+++ client/src/Components/Landing/landing.js
@@ -1,28 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../../Styles/landing.module.css";
 import logoText from "../../content/logoComplete.png";
 import henryShirt from "../../content/henryShirt.png";
+import Product from '../Product/product.js'
 
 export default function Landing(props) {
+  const [allProducts, setProducts] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:3000/products/')
+    .then((data) => data.json())
+    .then((data) => setProducts(data))
+    .catch((err) => console.log(err));
+  }, [])
   return (
     <body>
       <section id="section-one">
         <div className={styles.textBox}>
           <h2>BIENVENIDO A</h2>
-          <img className={styles.henryLogo} src={logoText} />
+          <img className={styles.henryLogo} src={logoText} alt='Logo Henry' />
           <p>STORE</p>
-          <a href="#">VER CATÁLOGO</a>
+          <a href="#section-two">VER CATÁLOGO</a>
         </div>
         <div className={styles.divFeaturedItem}>
-          <img src={henryShirt} className={styles.henryShirt} />
+          <img src={henryShirt} className={styles.henryShirt} alt='Remera Henry'/>
           <span>
             <p>Remera deportiva HENRY</p>
           </span>
         </div>
-        <footer>{/* <img src={}/> */}</footer>
       </section>
-      <section id="section-two">
-
+      <section id="section-two" className={styles.productSection}>
+        <span><h1>CATÁLOGO</h1></span>
+        {allProducts.map((el)=> {
+          return <Product product={el}/>
+        })}
       </section>
     </body>
   );
