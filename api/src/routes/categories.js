41e3f5fc4f90f99d828a5cfbc6d9537e400d0e84
Matias Bonino <matias2301@hotmail.com>
2020-09-08 23:48:54 -0300

    Se movieron rutas de categorias a su archivo correspondiente

--- api/src/routes/categories.js
+++ api/src/routes/categories.js
@@ -10,4 +10,52 @@ server.get("/", (req, res, next) => {
     .catch(next);
 });
 
+
+//Trae todos los productos de una categoria
+server.get("/category/:name", (req, res, next) => {
+  const name = req.params.name;
+  Categories.findOne({
+    where: {
+      name: {
+        [Op.iLike]: `%${name}%`,
+      },
+    },
+    include: Product,
+  })
+    .then((rows) => res.status(200).json(rows))
+    .catch(next);
+});
+
+
+//Elimina una categoria segun su id
+server.delete("/category/:id", (req, res, next) => {
+  const id = req.params.id;
+
+  Categories.destroy({ where: { id } })
+    .then((rows) => res.status(200).json(rows))
+    .catch(next);
+});
+
+
+//Crea una nueva categoria
+server.post("/create-category", (req, res, next) => {
+  
+  Categories.findOrCreate({
+    where: {
+      name: req.body.name,
+      description: req.body.description,
+    },
+  })
+    .then((category) => {
+      //Sucess handler
+      res
+        .status(200)
+        .send(`La categoría ${category[0].dataValues.name} se creó con exito`);
+    })
+    .catch((err) => {
+      //Error Handler
+      res.status(400).send("No se pudo crear la categoría solicitada");
+    });
+});
+
 module.exports = server;
