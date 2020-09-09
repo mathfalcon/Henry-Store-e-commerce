de11bb319c674c3b5286fa9627bdfb4b7fbaff8b
Santi-Sosa <santisose01@gmail.com>
2020-09-09 03:02:20 -0300

    Finalizado S35-Crear-Ruta-para-modificar-Usuario

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -3,9 +3,31 @@ const { Users } = require("../db.js");
 
 // Busca todos los usuarios y los devuelve en un array
 server.get("/", (req, res, next) => {
-  Product.findAll()
+  Users.findAll()
     .then((users) => {
       res.status(200).send(users);
     })
     .catch(next);
+});
+
+//Modifica un usuario pasado por id (query)
+server.put("/:id", (req, res, next) => {
+
+  const { name, username, email } = req.body;
+
+  Users.findByPk(req.params.id)
+    .then((data) => {
+      if (name) data.name = name;
+      if (username) data.username = username;
+      if (email) data.email = email;
+      data.save();
+      res
+        .status(200)
+        .send(
+          `El usuario ${data.dataValues.name} con id ${data.dataValues.id} se actualizó con éxito`
+        );
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
 });
