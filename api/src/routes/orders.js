2988e3205fdb36618b277f5724c4990cebf4db52
Agustin Dominguez <gusdomdominguez@gmail.com>
2020-09-11 01:54:45 -0300

    Terminada S46 Crear Ruta que retorne una orden en particular

--- api/src/routes/orders.js
+++ api/src/routes/orders.js
@@ -1,5 +1,5 @@
 const server = require("express").Router();
-const { Orders } = require("../db.js");
+const { Order } = require("../db.js");
 
 // Busca todas las ordenes y los devuelve en un array y filtra si posee query con status
 //no se puede testear por que no existe ruta para crear ordenes
@@ -7,18 +7,24 @@ server.get("/", (req, res, next) => {
 
     var status = req.query.status;
     if(!status){
-        Orders.findAll()
+        Order.findAll()
         .then((orders) => {
           res.status(200).send(orders);
         })
         .catch(next);
     }else{
-        Orders.findAll({ where: {state : status} })
+        Order.findAll({ where: {state : status} })
             .then((orders) => {
                 res.status(200).send(orders);
             })
           .catch(next);
         }
         });
+
+        server.get("/:idOrder", (req, res, next) => {
+          Order.findByPk(req.params.idOrder)
+          .then((order) => res.send(order))
+          .catch(next);
+              });
 
 module.exports = server;
