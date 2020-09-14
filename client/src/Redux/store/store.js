6650c135bfe639af923efeed2a7b42bb129b1e68
Matias Bonino <matias2301@hotmail.com>
2020-09-14 01:32:22 -0300

    Se agrego ruta para el admin

--- client/src/Redux/store/store.js
+++ client/src/Redux/store/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware, combineReducers, compose } from "redux";
-import { orderListReducer } from '../reducers/mainReducer'
+import { orderListReducer } from '../reducers/orderReducer'
+import { getLoggedUserReducer } from '../reducers/userReducer';
 import thunk from "redux-thunk";
 
 const reducer = combineReducers({
     orderList: orderListReducer,
+    getLoggedUser: getLoggedUserReducer,
 });
 
 // const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() || compose;
